d00d709106848d82351fc70a0b0d8fcb399e0dcc
Andry <asitrakiniainasoa@gmail.com>
2022-10-22 21:21:03 +0300

    add filter in eshop

--- src/pages/eshop/index.tsx
+++ src/pages/eshop/index.tsx
@@ -1,4 +1,5 @@
 import ArrowDown from "../../components/arrow-down";
+import Filter from "./filter";
 import Header from "./header";
 import "./style.scss";
 
@@ -7,6 +8,12 @@ const EShop = () => {
     <div>
       <Header />
       <ArrowDown />
+      <section className="row p-4">
+        <article className="col-3">
+          <Filter />
+        </article>
+        <article className="col-9">{/* <app-results></app-results> */}</article>
+      </section>
     </div>
   );
 };
